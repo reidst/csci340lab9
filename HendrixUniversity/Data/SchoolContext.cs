3a5b8cc0ca510566b858101ddf14840f4cd06989
reidst <reidst@hendrix.edu>
2023-10-27 12:49:44 -0500

    updated database context class

--- HendrixUniversity/Data/SchoolContext.cs
+++ HendrixUniversity/Data/SchoolContext.cs
@@ -13,7 +13,15 @@ namespace HendrixUniversity.Data
             : base(options)
         {
         }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
 
-        public DbSet<HendrixUniversity.Models.Student> Student { get; set; } = default!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
     }
 }
