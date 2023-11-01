cb4d75d13a52780246a1638f59470f19a9be755c
reidst <reidst@hendrix.edu>
2023-10-31 22:46:43 -0500

    added about page

---
+++ HendrixUniversity/Models/SchoolViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HendrixUniversity.Models.SchoolViewModels
+{
+    public class EnrollmentDateGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? EnrollmentDate { get; set; }
+
+        public int StudentCount { get; set; }
+    }
+}
