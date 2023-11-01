85873f2374499e3bb9288699d37fb0e6cb0f4ec8
reidst <reidst@hendrix.edu>
2023-11-01 13:13:37 -0500

    created complex data model

--- HendrixUniversity/Migrations/20231101180821_ComplexDataModel.Designer.cs
+++ HendrixUniversity/Migrations/20231101180821_ComplexDataModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HendrixUniversity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HendrixUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20231101180821_ComplexDataModel")]
+    partial class ComplexDataModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.12");
