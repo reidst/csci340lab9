7ffbfba18bbbd067cdb6e86615bb66a7b4be04d8
reidst <reidst@hendrix.edu>
2023-11-01 23:11:54 -0500

    updated instructor pages

--- HendrixUniversity/Pages/Instructors/Delete.cshtml.cs
+++ HendrixUniversity/Pages/Instructors/Delete.cshtml.cs
@@ -48,15 +48,24 @@ namespace HendrixUniversity.Pages.Instructors
             {
                 return NotFound();
             }
-            var instructor = await _context.Instructors.FindAsync(id);
+            
+            Instructor instructor = await _context.Instructors
+                .Include(i => i.Courses)
+                .SingleAsync(i => i.ID == id);
 
-            if (instructor != null)
+            if (instructor == null)
             {
-                Instructor = instructor;
-                _context.Instructors.Remove(Instructor);
-                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
             }
 
+            var departments = await _context.Departments
+                .Where(d => d.InstructorID == id)
+                .ToListAsync();
+            departments.ForEach(d => d.InstructorID = null);
+
+            _context.Instructors.Remove(instructor);
+
+            await _context.SaveChangesAsync();
             return RedirectToPage("./Index");
         }
     }
