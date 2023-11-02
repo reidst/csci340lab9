6270be9e8d7da54fa18ef9200831dbb91df54da1
reidst <reidst@hendrix.edu>
2023-11-01 23:01:00 -0500

    updated course pages

--- HendrixUniversity/Pages/Courses/Delete.cshtml.cs
+++ HendrixUniversity/Pages/Courses/Delete.cshtml.cs
@@ -29,15 +29,14 @@ namespace HendrixUniversity.Pages.Courses
                 return NotFound();
             }
 
-            var course = await _context.Courses.FirstOrDefaultAsync(m => m.CourseID == id);
+            Course = await _context.Courses
+                .AsNoTracking()
+                .Include(c => c.Department)
+                .FirstOrDefaultAsync(m => m.CourseID == id);
 
-            if (course == null)
+            if (Course == null)
             {
                 return NotFound();
-            }
-            else 
-            {
-                Course = course;
             }
             return Page();
         }
